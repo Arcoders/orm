f01e8de0568f6255907eabecd1cd97f56c330f69
Ismael Haytam <anonismaa@gmail.com>
2017-04-04 04:54:18 +0200

    orderBy, take, get

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,10 +3,12 @@
 namespace ORM\Http\Controllers;
 
 use Illuminate\Http\Request;
+use ORM\User;
 
 class PagesController extends Controller
 {
     public function home() {
-        return view('pages.home');
+        $users = User::orderBy('id', 'DESC')->take(10)->get();
+        return view('pages.home', compact('users'));
     }
 }
